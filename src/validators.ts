ec619019431c0e6d7696c2b6bca123817d79cc15
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2025-02-02 19:33:16 +0300

    changes

--- src/validators.ts
+++ src/validators.ts
@@ -12,79 +12,70 @@ export const userSchema = z.object({
 })
 
 export const vehicleSchema=z.object({
-    owner_id:z.number(),
+    engine_capacity:z.string(),
     make:z.string(),
     model:z.string(),
     year:z.number(),
-    vin:z.number(),
+    transmission:z.string(),
     status:z.string(),
     price:z.number(),
-    milleage:z.number(),
+    seating_capacity:z.number(),
     fuel_type:z.string(),
-    location:z.string(),
-    image:z.number(),
+    color:z.string(),
+    location_id:z.number(),
+    exterior_image:z.string(),
+    interior_image:z.string(),
+    history:z.string(),
     created_at:z.string().optional(),
     updated_at:z.string().optional()
 })
 export const landSchema=z.object({
-    owner_id:z.number(),
-    size:z.number(),
+    history:z.string(),
+    size:z.string(),
     status:z.string(),
     land_type:z.string(),
     price:z.number(),
-    location:z.string(),
+    location_id:z.number(),
     image:z.number(),
     created_at:z.string().optional(),
     updated_at:z.string().optional()
 })
 export const houseSchema=z.object({
-    owner_id:z.number(),
-    address:z.string(),
-    name:z.string(),
+    location_id:z.number(),
     number_of_rooms:z.number(),
-    size:z.number(),
+    number_of_bedrooms:z.number(),
+    type:z.string(),
     status:z.string(),
     price:z.number(),
     year_built:z.number(),
-    image:z.number(),
+    exterior_image:z.string(),
+    interior_image:z.string(),
+    history:z.string(),
     created_at:z.string().optional(),
     updated_at:z.string().optional()
 })
 export const locationSchema = z.object({
-    city: z.string(),
-    state: z.string(),
-    country: z.string(),
-    address:z.string(),
-    zip_code: z.string(),
+    address: z.string(),
+    name: z.string(),
+    contact:z.string(),
     created_at:z.string().optional(),
     updated_at:z.string().optional()
 })
-export const historySchema = z.object({
-    property_type: z.string(),
-    property_id: z.number(),
-    event: z.string(),
-    event_date:z.string().optional(),
-    description: z.string()
-})
-export const bookingsSchema = z.object({
-    property_type: z.string(),
-    property_id: z.number(),
+export const purchaseSchema = z.object({
+    purchase_status: z.string().optional(),
+    total_amount: z.number(),
     user_id: z.number(),
-    status:z.string(),
-    booking_date:z.string().optional()
+    location_id:z.number(),
+    purchase_date:z.string().optional()
 })
 export const transactionSchema = z.object({
-    property_type: z.string(),
-    property_id: z.number(),
-    booking_id: z.number(),
-    buyer_id:z.number(),
-    sale_price: z.number(),
+    purchase_id: z.number(),
+    user_id: z.number(),
+    amount:z.number(),
     status: z.string(),
     transaction_date:z.string().optional()
 })
 export const reviewsSchema = z.object({
-    property_type: z.string(),
-    property_id: z.number(),
     user_id: z.number(),
     rating:z.number(),
     comment: z.string(),
