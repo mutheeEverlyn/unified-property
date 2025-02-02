8e116a18a0b0469957859722e351887bd117d025
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2025-02-02 19:33:26 +0300

    changes

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,67 @@
+import { AuthOnUsersTable, usersTable } from "../drizzle/schema";
+import db from "../drizzle/db";
+import { z } from 'zod';
+import { sql } from "drizzle-orm";
+
+const registrationSchema = z.object({
+  full_name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+  contact_phone: z.string(),
+  address: z.string(),
+  role: z.enum(["admin", "user", "userAdminRoleAuth"]).optional(),
+});
+
+type RegistrationInfo = z.infer<typeof registrationSchema>;
+
+export const createAuthUserService = async (user: RegistrationInfo): Promise<number | null> => {
+  const parsedData = registrationSchema.parse(user);
+
+  const userRecord = await db.insert(usersTable).values({
+    full_name: parsedData.full_name,
+    email: parsedData.email,
+    contact_phone: parsedData.contact_phone,
+    address: parsedData.address,
+    role: parsedData.role,
+    created_at: new Date().toISOString(),
+    updated_at: new Date().toISOString()
+  }).returning({ user_id: usersTable.user_id });
+
+  const user_id = userRecord[0]?.user_id;
+  if (!user_id) return null;
+
+  await db.insert(AuthOnUsersTable).values({
+    user_id: user_id,
+    password: parsedData.password,
+    created_at: new Date().toISOString(),
+    updated_at: new Date().toISOString()
+  });
+
+  return user_id;
+};
+
+export interface UserDetails {
+  password: string;
+  email: string;
+}
+
+export const userLoginService = async (user: UserDetails) => {
+  const {email, password } = user;
+  return await db.query.usersTable.findFirst({
+    columns: {
+      user_id:true,
+      email:true,
+      full_name:true,
+      role:true
+    },
+    where: sql`${usersTable.email} = ${email}`,
+    with: {
+      user: {
+        columns: {
+       password:true
+        }
+      }
+    }
+  });
+
+};
